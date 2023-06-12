88ba7d673e2005caf7eb4c7d42f1bea16a846ce4
Jonnathan <jonnabcl56@gmail.com>
2023-06-12 08:53:04 -0500

    Controllers

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -4,13 +4,15 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticlesController extends AbstractController
 {
     #[Route(path: '/articles', name: 'articles', methods: ['GET'])]
-    public function list(): Response
+    public function list(Request $request): Response
     {
-        return new Response('Welcome to Latte and Code ');
+        $name = $request->query->get('name', 'Jonnathan');
+        return new Response('Welcome to Latte and Code ' . $name);
     }
 }
